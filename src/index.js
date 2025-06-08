d284dc9cea5662274c1afe1be41192d4148aabea
yuriy6971 <yuriy6971@ukr.net>
2025-06-08 20:03:41 +0300

    replace Browserrouter on HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import store from "./redux/redux_store";
 import { Provider } from "react-redux";
@@ -9,9 +9,9 @@ import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
